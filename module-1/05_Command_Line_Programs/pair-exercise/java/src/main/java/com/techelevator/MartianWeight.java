049252c3ebe02a35ce36ca388d7633ba0541fdff
Kevin Ventura <venke12@gmail.com>
2020-01-17 14:18:34 -0500

    Week 1 Team 8 Martian Weight Exercise

--- module-1/05_Command_Line_Programs/pair-exercise/java/src/main/java/com/techelevator/MartianWeight.java
+++ module-1/05_Command_Line_Programs/pair-exercise/java/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.util.Scanner;
 /*
  In case you've ever pondered how much you weight on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -20,6 +21,19 @@ public class MartianWeight {
 
 	public static void main(String[] args) {
 
-	}
+		Scanner input= new Scanner(System.in);
+		double [] earthWeights = new double[3];
+		
+		System.out.print("Enter three Earth weights [separated by spaces]: ");
+		for (int x=0; x < earthWeights.length; x++) {
+			String weightsStr = input.next();
+			String [] weightsSplit = weightsStr.split(" ");
+			earthWeights[x] = Double.parseDouble(weightsStr);
+		}
+		for (int x=0; x < earthWeights.length; x++) {
+			double marsWeights = earthWeights[x]*0.378;
+			System.out.println(String.format("%8.1f",earthWeights[x])+ " lbs. on Earth is equal to "+ String.format("%8.1f",marsWeights)+ " lbs. on Mars");	
+		}
 
+	}
 }
